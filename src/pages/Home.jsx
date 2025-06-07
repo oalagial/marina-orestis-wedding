4696a6684c25c25c93ac409879fd6037f86f9539
orestisalagialoglou <orestisal97@gmail.com>
2025-06-07 15:12:18 +0100

    feat: update font imports and apply new font styles in Home page for enhanced aesthetics

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -14,15 +14,14 @@ const Home = () => (
                 className="hidden md:block md:w-1/3 h-full object-cover"
             />
         </div>
-                {/* <h1 className="mt-8 text-3xl md:text-5xl font-serif font-bold text-gray-800 tracking-wide"> */}
-        <h1 className="font-cursive">
+        <h1 className="mt-8 text-3xl md:text-5xl font-serif font-bold text-gray-800 tracking-wide">
             Welcome to Our Wedding
         </h1>
         <p className="mt-4 text-lg md:text-xl text-gray-600 font-light italic font-extrabold">
             We are delighted to invite you to celebrate our special day with us!
         </p>
-        <h2 className="mt-8 text-2xl md:text-3xl font-cursive font-semibold text-gray-500 tracking-wide">
-            <span className="italic">Marina</span> <span className="text-gray-400">&amp;</span> <span className="italic">Orestis</span>
+        <h2 className="mt-8 text-2xl md:text-3xl custom-dancing-font tracking-wide">
+            <span className="italic">Marina</span> <span className="">&amp;</span> <span className="italic">Orestis</span>
         </h2>
         <p className="mt-4 text-base md:text-lg text-gray-700 font-medium">
             <span className="font-serif">Date:</span>{' '}
