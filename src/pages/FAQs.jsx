1dac3a86bbcdcdc149f9a234a5b86949df70b12d
orestisalagialoglou <orestisal97@gmail.com>
2026-03-03 15:28:29 +0000

    feat: Add Link component import to FAQs page for navigation enhancements

--- src/pages/FAQs.jsx
+++ src/pages/FAQs.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { useTranslation } from 'react-i18next';
+import { Link } from 'react-router-dom';
 
 const FAQs = () => {
     const { t } = useTranslation();
